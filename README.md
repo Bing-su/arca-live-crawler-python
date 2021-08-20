9a8f13dba216c092c82021eb0251c7c634b30a5d
Bingsu <ks2515@naver.com>
2021-08-20 21:49:26 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-arca-live-crawler-python
+# arca-live-crawler-python
+
+### 아카라이브에서 댓글을 크롤링하는 파이썬 프로그램입니다.
+
+제목, 내용도 일단 수집하지만 사용하지는 않습니다.
+
+## 사용방법
+
+예) `python main.py genshin 1 2 -b`  
+ -> 원신 채널의 개념글에서 1페이지부터 2페이지까지 크롤링한다.
+
+</br>
+
+`python main.py "채널 이름(영어)" "시작페이지" "끝페이지"`  
+`-b, --best` (옵션) 사용하면 개념글 페이지에서 크롤링합니다.  
+`--file_name` (옵션) 크롤링할 댓글을 저장할 파일 이름. 자동으로 뒤에 .csv가 붙습니다.  
+`--path` (옵션) 파일을 저장할 경로. 기본적으로 현재 위치에 data폴더를 만들어 저장합니다.
+
+## 요구사항
+`requests`  
+`beautifulsoup4`  
+`lxml`  
+`tqdm`
---
+++ main.py
@@ -0,0 +1,65 @@
+import argparse
+import csv
+from datetime import datetime
+import os
+import random
+import time
+
+from tqdm.auto import tqdm, trange
+
+from functions import get_post_urls, get_contents
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("channel", type=str, help="아카라이브의 채널명(영어)")
+parser.add_argument("start", type=int, help="크롤링을 시작할 페이지 번호")
+parser.add_argument("end", type=int, help="크롤링을 끝낼 페이지 번호")
+parser.add_argument(
+    "-b", "--best", action="store_true", help="이 옵션을 사용하면 개념글에서 페이지를 크롤링합니다."
+)
+parser.add_argument("--file_name", type=str, help="결과물 파일의 이름")
+parser.add_argument("--path", type=str, help="결과물 파일을 저장할 경로")
+
+args = parser.parse_args()
+
+# 파일 이름 정하기
+if args.file_name is None:
+    t = datetime.now()
+    now = f"{t.year % 100}{t.month:02}{t.day:02}-{t.hour:02}{t.minute:02}{t.second:02}"
+    file_name = f"{args.channel}_{now}.csv"
+else:
+    file_name = args.file_name + ".csv"
+
+# 저장 경로 정하기
+if args.path is None:
+    if not os.path.isdir("data"):
+        os.mkdir("data")
+    path = "./data"
+else:
+    path = args.path
+
+
+file = open(os.path.join(path, file_name), "w", encoding="utf-8", newline="")
+writer = csv.writer(file)
+writer.writerow(["content", "label"])
+
+# 크롤링 시작
+comments_count = 0
+for page in trange(args.start, args.end + 1, desc="page"):
+    urls = get_post_urls(args.channel, page, args.best)
+
+    for url in tqdm(urls, desc="post", leave=False):
+        contents = get_contents(url)
+
+        title = contents["title"]
+        content = contents["content"]
+        comments = contents["comments"]
+
+        for comment in comments:
+            writer.writerow([comment, 0])
+            comments_count += 1
+
+    time.sleep(0.423 + random.random())
+
+file.close()
+print(f"완료, 크롤링한 댓글 수: {comments_count}")
